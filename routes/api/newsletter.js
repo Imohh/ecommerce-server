b9c24e1cf92ecbca89d751c0f31bafd75842cd75
Imoh Precious <jaypee88830@gmail.com>
2023-09-06 04:30:54 +0100

    newsletter saved to database

--- routes/api/newsletter.js
+++ routes/api/newsletter.js
@@ -4,25 +4,27 @@ const router = express.Router();
 const mailchimp = require('../../services/mailchimp');
 const mailgun = require('../../services/mailgun');
 
+const Newsletter = require('../../models/Newsletter');
+
 router.post('/subscribe', async (req, res) => {
-  const email = req.body.email;
+  try {
+    const email = req.body.email;
+    const formEntry = new Newsletter({
+      email,
+    });
 
-  if (!email) {
-    return res.status(400).json({ error: 'You must enter an email address.' });
+    if (!email) {
+      return res.status(400).json({ error: 'You must enter an email address.' });
+    }
+
+    await formEntry.save();
+    res.status(200).json({ message: 'Form data saved successfully' });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
   }
-
-  const result = await mailchimp.subscribeToNewsletter(email);
-
-  if (result.status === 400) {
-    return res.status(400).json({ error: result.title });
-  }
-
-  await mailgun.sendEmail(email, 'newsletter-subscription');
-
-  res.status(200).json({
-    success: true,
-    message: 'You have successfully subscribed to the newsletter'
-  });
+  
 });
 
 module.exports = router;
