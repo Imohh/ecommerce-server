7fb20c18cc669976254e311de1e942e341c95210
Imoh Precious <jaypee88830@gmail.com>
2023-09-06 22:59:33 +0100

    newsletter test again

--- models/newsletter.js
+++ models/newsletter.js
@@ -1,10 +1,27 @@
-const mongoose = require('mongoose');
-const {Schema, model} = mongoose;
+// const mongoose = require('mongoose');
+// const {Schema, model} = mongoose;
 
+// const NewsletterSchema = new Schema({
+//   email: {type: String, required: true},
+// });
+
+// const NewsletterModel = model('Newsletter', NewsletterSchema);
+
+// module.exports = NewsletterModel;
+
+
+const Mongoose = require('mongoose');
+const { Schema } = Mongoose;
+
+// Contact Schema
 const NewsletterSchema = new Schema({
-  email: {type: String, required: true},
+  email: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-const NewsletterModel = model('Newsletter', NewsletterSchema);
-
-module.exports = NewsletterModel;
+module.exports = Mongoose.model('Newsletter', NewsletterSchema);
--- routes/api/newsletter.js
+++ routes/api/newsletter.js
@@ -1,28 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-const mailchimp = require('../../services/mailchimp');
-const mailgun = require('../../services/mailgun');
+// const mailchimp = require('../../services/mailchimp');
+// const mailgun = require('../../services/mailgun');
+
+const Newsletter = require('../../models/newsletter');
 
 router.post('/subscribe', async (req, res) => {
-  const email = req.body.email;
+  try {
+    const email = req.body.email;
 
-  if (!email) {
-    return res.status(400).json({ error: 'You must enter an email address.' });
+    if (!email) {
+      return res.status(400).json({ error: 'You must enter an email address.' });
+    }
+
+    const newsletter = new Newsletter({
+      email
+    });
+
+    const newsletterDoc = await newsletter.save();
+    res.status(200).json({
+      success: true,
+      message: `We receved your email, we will reach you on your email address ${email}!`,
+      newsletter: newsletterDoc
+    });
+
+  } catch (error) {
+    return res.status(400).json({
+      error: 'Your request could not be processed. Please try again.'
+    });
   }
-
-  const result = await mailchimp.subscribeToNewsletter(email);
-
-  if (result.status === 400) {
-    return res.status(400).json({ error: result.title });
-  }
-
-  await mailgun.sendEmail(email, 'newsletter-subscription');
-
-  res.status(200).json({
-    success: true,
-    message: 'You have successfully subscribed to the newsletter'
-  });
+  
 });
 
 module.exports = router;
