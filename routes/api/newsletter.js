c0e31d76b5cae6400e972e9a895e0fbd80f10f2c
Imoh Precious <jaypee88830@gmail.com>
2023-09-06 22:10:13 +0100

    newsletter test

--- routes/api/newsletter.js
+++ routes/api/newsletter.js
@@ -29,21 +29,3 @@ router.post('/subscribe', async (req, res) => {
 });
 
 module.exports = router;
-
-// try {
-//     const email = req.body.email;
-//     const formEntry = new Newsletter({
-//       email,
-//     });
-
-//     if (!email) {
-//       return res.status(400).json({ error: 'You must enter an email address.' });
-//     }
-
-//     await formEntry.save();
-//     res.status(200).json({ message: 'Form data saved successfully' });
-
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
