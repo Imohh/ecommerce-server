9f2f90d610fa6967dee250b69feef824e1fbea17
Imoh Precious <jaypee88830@gmail.com>
2023-09-23 11:28:52 +0100

    server links in stripe route

--- routes/api/stripe.js
+++ routes/api/stripe.js
@@ -36,8 +36,8 @@ router.post('/create-checkout-session', async (req, res) => {
               },
             ],
             mode: 'payment',
-            success_url: 'http://localhost:8080/success',
-            cancel_url: 'http://localhost:8080/contact',
+            success_url: 'https://eminencebygtx.com/success',
+            cancel_url: 'https://eminencebygtx.com/contact',
             payment_intent_data: {
                 description: paymentDescription,
             }
