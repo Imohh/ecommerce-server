6d9a47ad8bfdfbb358ec020284a3ae81a655d8f0
Imoh Precious <jaypee88830@gmail.com>
2023-09-29 14:41:15 +0100

    newsletter route

--- routes/api/newsletter.js
+++ routes/api/newsletter.js
@@ -1,37 +1,76 @@
 const express = require('express');
 const router = express.Router();
 
-// const mailchimp = require('../../services/mailchimp');
-// const mailgun = require('../../services/mailgun');
+const mailchimp = require('../../services/mailchimp');
+const mailgun = require('../../services/mailgun');
+// const { sendWelcomeEmail } = require('../../services/email')
 
 const Newsletter = require('../../models/newsletter');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+function generateCouponCode() {
+  const timestamp = Date.now(); // Get the current timestamp in milliseconds
+  const randomString = Math.random().toString(36).substring(2, 8); // Generate a random alphanumeric string
+  const couponCode = randomString; // Combine timestamp and random string
+  return couponCode;
+}
 
 router.post('/subscribe', async (req, res) => {
   try {
     const email = req.body.email;
 
+
     if (!email) {
       return res.status(400).json({ error: 'You must enter an email address.' });
     }
 
-    const newsletter = new Newsletter({
-      email
-    });
+    const existingSubscriber = await Newsletter.findOne({ email });
 
-    const newsletterDoc = await newsletter.save();
-    res.status(200).json({
-      success: true,
-      message: `We receved your email, we will reach you on your email address ${email}!`,
-      newsletter: newsletterDoc
+    if (existingSubscriber) {
+      return res.status(400).json({ error: 'This email is already subscribed.' });
+    }
+
+
+    const couponCode = generateCouponCode()
+    console.log('Generated coupon code:', couponCode)
+
+    const formEntry = new Newsletter({
+      email,
+      couponId: couponCode
     });
+    
+
+    const coupon = await stripe.coupons.create({
+      percent_off: 10,
+      duration: 'once',
+      id: couponCode,
+    })
+
+    // Create a Stripe customer
+    const customer = await stripe.customers.create({
+      email: email,
+      description: 'Newsletter Subscriber',
+      coupon: coupon.id,
+    });
+    
+    const promotionCode = await stripe.promotionCodes.create({
+      coupon: couponCode,
+      code: 'EMINENCEBYGTX',
+      customer: customer.id,
+    });
+    
+    await formEntry.save();
+
+    // Send a welcome email to the use after saving the subscription
+    // await sendWelcomeEmail(email)
+
+    res.status(200).json({ message: 'Form data saved successfully' });
 
   } catch (error) {
-    return res.status(400).json({
-      error: 'Your request could not be processed. Please try again.'
-    });
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
   }
   
 });
 
 module.exports = router;
-
