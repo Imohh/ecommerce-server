f485c41a55769d0224b75c86dc68d8c9fcdb8baf
Imoh Precious <jaypee88830@gmail.com>
2023-09-29 14:03:32 +0100

    stripe route

--- routes/api/stripe.js
+++ routes/api/stripe.js
@@ -8,7 +8,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 router.post('/create-checkout-session', async (req, res) => {
     try {
 
-        const { total, productNames } = req.body;
+        const { total, productNames, user } = req.body;
         const email = user.email
 
         const combineProductNames = productNames.join(', ');
